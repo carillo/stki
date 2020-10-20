bf723b246acb5ce8d88ae5af98bfdb4afb644faa
carillo <59461526+carillo@users.noreply.github.com>
2020-10-20 11:46:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -14,15 +14,9 @@ h2 {
 <hr>
 <div align=center>
 | <a href="koneksi.php">Koneksi</a> |
-<a href="buatberita.php">Buat Dokumen</a> |
-<a href="lihatberita.php">Lihat Dokumen</a> |
-<a href="lowercase.php">Lower Case Dokumen</a> |
-<a href="hapustandabaca.php">Hapus Tanda Baca</a> |
-<a href="tokenisasi.php">Tokenisasi Kata</a> |
-<a href="hasiltokenisasikata.php">Hasil Token Kata</a> |
-<a href="datastopword.php">Data Stopword</a> |
-<a href="prosesstopword.php">Proses Stopword</a> |
-<a href="prosesstemming.php">Proses Stemming</a> |
+<a href="tf.php">Term Frequency</a> |
+<a href="idf.php">Invers Document Frequency</a> |
+<a href="tfidf.php">TF.IDF</a> |
 </div>
 <hr/>
 </body>
--- koneksi.php
+++ koneksi.php
@@ -3,10 +3,10 @@
 
 $host  = "localhost";
 $username  = "root";
-$password  = "";
-$database  = "stki";
+$password  = "root";
+$database  = "tfidf";
 $koneksi  = mysqli_connect($host,  $username,  $password, $database);
-$pilihdatabase  = mysqli_select_db($koneksi, "stki");
+$pilihdatabase  = mysqli_select_db($koneksi, "tfidf");
 if  ($pilihdatabase) echo "<h3>Koneksi Berhasil </h3><hr><br/>";
 else echo "Koneksi Gagal <br/>";
 ?>
